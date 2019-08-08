31b4aa5189e2a6f1a6376f4022aa3359af9b21f7
Albert Papp <albert.papp@blacksalt.io>
2019-08-08 08:57:39 +0200

    Formatting changes

--- lib/model/pixabay_media.dart
+++ lib/model/pixabay_media.dart
@@ -200,7 +200,6 @@ class PixabayImage extends PixabayMedia {
 
   @override
   String getThumbnailLink() {
-
     return getDownloadLink(res: Resolution.tiny);
   }
 
@@ -331,7 +330,6 @@ class PixabayVideo extends PixabayMedia {
 
   @override
   String getThumbnailLink() {
-   
     String cdnLink = "https://i.vimeocdn.com/video/";
     String cdnExtension = ".jpg";
     String res = "_640x360";
